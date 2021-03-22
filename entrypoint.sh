03b14b6451c5bd158854fe7e2c0ffef28096d6f7
Jakub Domagala <domagala.jacob@gmail.com>
2021-03-23 00:16:54 +0100

    #5: Update entrypoint to build and push docker image. It also extracts the last build time from docker image and passes it through to python script

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,6 +17,15 @@ if [ -z "${INPUT_GITHUB_PERSONAL_TOKEN}" ]; then
     exit 1
 fi
 
+docker build -t ${INPUT_DOCKER_REPOSITORY}:${INPUT_TAG} .
+
+containerId=$(docker create ${INPUT_DOCKER_REPOSITORY}:${INPUT_TAG})
+docker cp "$containerId":"${INPUT_LAST_BUILD_TIME_FILE}" ./last_build.txt
+docker rm "$containerId"
+
+# echo "${INPUT_DOCKER_PASSWORD}" | docker login -u ${INPUT_DOCKER_USERNAME} --password-stdin docker.io
+# docker push ${INPUT_DOCKER_REPOSITORY}:latest
+
 GIT_REPOSITORY_URL="https://${INPUT_GITHUB_PERSONAL_TOKEN}@github.com/$GITHUB_REPOSITORY.wiki.git"
 
 tmp_dir=$(mktemp -d -t ci-XXXXXXXXXX)
@@ -27,12 +36,15 @@ tmp_dir=$(mktemp -d -t ci-XXXXXXXXXX)
     git config user.email "$GITHUB_ACTOR@users.noreply.github.com"
     git pull "$GIT_REPOSITORY_URL"
 
-    # Generate graph
-    python3 /generate_graph.py
+    build_time=$(ls -l | grep -oP 'real\s+\K\d+m\d+\.\d+s' "$GITHUB_WORKSPACE"/last_build.txt)
+    echo $build_time
 
-    git add .
-    git commit -m "$INPUT_COMMIT_MESSAGE"
-    git push --set-upstream "$GIT_REPOSITORY_URL" master
+    # Generate graph
+    python3 /generate_graph.py -t $build_time -r $INPUT_RUN_NUMBER
+
+    # git add .
+    # git commit -m "$INPUT_COMMIT_MESSAGE"
+    # git push --set-upstream "$GIT_REPOSITORY_URL" master
 ) || exit 1
 
 rm -rf "$tmp_dir"
